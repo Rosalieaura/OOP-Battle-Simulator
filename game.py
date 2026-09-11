bc6f31a81a3d69b510298f24a46a4686ec609620
Rosalieaura <raryancry@gmail.com>
2026-09-11 09:29:31 -0500

    add a new gobblin

--- game.py
+++ game.py
@@ -15,6 +15,8 @@ def main():
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
 
+    NewGobblin= Goblin("house")
+
 
 if __name__ == "__main__":
     main()
